1f08342060ec714b7b62b34bb1a9e4b51f4c5f7c
Bohdan Onatsky <15bogdan17@gmail.com>
2019-11-14 23:05:34 +0200

    test

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,11 @@ const app = express();
 app.use('/', (req: express.Request, res: express.Response) => {
   res.json({ status: 200 });
 });
-
+app.use('/', (req: express.Request, res: express.Response) => {
+  res.json({ status: 200 });
+});
 app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
+  // eslint-disable-next-line no-consoles
   console.log(`Server running on ${PORT} port test`);
 });
 
